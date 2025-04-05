cbb0e7916fe6be40421b84612ce1f36acf3bec90
Diego <die10.2005@gmail.com>
2025-04-05 17:53:08 -0300

    Desafío práctico
    Se genera una clase StateFulWidget que dentro contiene un container
    el mismo va cambiando mediante un boton flotante, esto con ayuda del
    asistente.
    Se crea manuealmente un segundoboton que resetea el color del contenedor
    a blanco.
    Se modifica manuealmente el tamaño del contenedor.
    Se modifica manuealmenteel color del texto perteneciente al container de
    colores.

--- lib/main.dart
+++ lib/main.dart
@@ -15,57 +15,91 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const ColorChangerScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class ColorChangerScreen extends StatefulWidget {
+  const ColorChangerScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ColorChangerScreen> createState() => _ColorChangerScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0; // Variable para el contador
-
-  void _incrementCounter() {
-    /*El setState se usa dentro de esta funcion para indicar a flutter que debe
-      redibujar la pantalla con el valor de _counter, si no se utiliza al aumen-
-      tar- el contador no se vera reflejado en la pantalla hasta que se haga un
-      hot reload */
+class _ColorChangerScreenState extends State<ColorChangerScreen> {
+  final List<Color> _colors = [
+    Colors.white,
+    Colors.blue,
+    Colors.red,
+    Colors.green,
+  ];
+  //Esta variable se usa para cambiar el color del texto cuando el container
+  //esta en blanco.
+  late Color colorTexto = Colors.black;
+  int _currentColorIndex = 0;
+  void _changeColor() {
+    if (_currentColorIndex == 3) {
+      _currentColorIndex += 1;
+    }
+    //El setState notifica a Flutter un cambio en el estado del widget. Dentro de
+    //él se realiza una operacion matemática que devuelve el index al que se
+    //debe acceder en el array de colores
     setState(() {
-      _counter++; // Incrementa el contador
+      _currentColorIndex = (_currentColorIndex + 1) % _colors.length;
+    });
+    if (_currentColorIndex != 0) {
+      colorTexto = Colors.white;
+    } else {
+      colorTexto = Colors.black;
+    }
+  }
+
+  void _resetColor() {
+    setState(() {
+      _currentColorIndex = 0;
+      colorTexto = Colors.black;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Mi App')),
+      appBar: AppBar(
+        title: const Text('Mi App'),
+        centerTitle: true,
+        titleTextStyle: TextStyle(
+          color: colorTexto,
+          fontSize: 20,
+          fontFamily: 'Roboto', // Especifica la fuente 'Roboto'
+          
+        ),
+        backgroundColor: _colors[_currentColorIndex],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(//El container se encarga de envolver al texto "Hola Flutter"
-              color: Colors.green,
-              padding: const EdgeInsets.all(20.0),
-              child: const Text(
-                'Hola, Flutter',
-                style: TextStyle(fontSize: 24),
+            Container(
+              width: 300,
+              height: 300,
+              color: _colors[_currentColorIndex],
+              alignment: Alignment.center,
+              child: Text(
+                '¡Cambio de color!',
+                style: TextStyle(fontSize: 18, color: colorTexto),
+                textAlign: TextAlign.center,
               ),
             ),
-            Text(
-              'Veces presionado: $_counter', // Muestra el valor del contador
-              style: const TextStyle(fontSize: 20,
-              color: Colors.blue),
-            ),
-            const SizedBox(height: 30), // Espacio entre el texto y el botón
+            const SizedBox(height: 20),
             ElevatedButton(
-              onPressed:
-                  _incrementCounter, // Llama a la función para incrementar
-              child: const Text('Presionar'),
+              onPressed: _changeColor,
+              child: const Text('Cambiar color'),
+            ),
+
+            ElevatedButton(
+              onPressed: _resetColor,
+              child: const Text('Resetear color'),
             ),
           ],
         ),
